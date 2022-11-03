a7195cd2ae0845768342f220a54a12dfe65067b2
Chiquil09 <114046386+Chiquil09@users.noreply.github.com>
2022-11-03 18:30:22 -0500

    Commit 1

---
+++ AEROPUERTO2/src/com/tuempresa/aeropuerto2/modelo/Persona.java
@@ -0,0 +1,29 @@
+package com.tuempresa.aeropuerto2.modelo;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@View(members = "nombres, apellidos; " +
+                              "fotografia;" +
+		                      "direccion")
+public class Persona extends Identificable {
+	
+	@Column(length = 23)
+	String nombres;
+	
+	@Column(length = 23)
+	String apellidos;
+	
+	@File
+	@Column(length = 32)
+	String fotografia;
+	
+	@ManyToOne
+	Direccion direccion;
+
+}
